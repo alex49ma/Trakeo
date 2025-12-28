2e2a0886912d07de20b6a0b37ea32b0a6b10e35e
Alejandro Martín <alejandrmartnaudn@gmail.com>
2025-12-28 02:18:02 +0100

    Hero start and banner

--- trakeo/src/app/page.js
+++ trakeo/src/app/page.js
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="mt-40">
-      <Button variant="destructive">Test</Button>
+      <HeroSection />
     </div>
   );
 }
---
+++ trakeo/src/components/hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+const HeroSection = () => {
+    return (
+        <div className="pb-20 px-4">
+            <div className="container mx-auto text-center">
+                <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">The tool to track </h1>
+                <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">your finances</h1>
+                <p>Track your expenses and income in one place<br />Analyze and get key insights</p>
+                <div>
+                    <Link href="/dashboard">
+                        <Button size="lg" variant="outline" className="px-8">Get Started</Button>
+                    </Link>
+                    <Link href="/">
+                        <Button size="lg" variant="outline" className="px-8">Watch Demo</Button>
+                    </Link>
+                </div>
+                <div>
+                    <div>
+                        <Image src="/banner.png"
+                            width={1280}
+                            height={720}
+                            alt="Dashboard Preview"
+                            className="rounded-lg shadow-2xl border mx-auto"
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
