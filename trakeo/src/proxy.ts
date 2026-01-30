55b08fe18772722f557b280adee1e4ee11af5c3f
Alejandro Martín <alejandrmartnaudn@gmail.com>
2026-01-30 19:32:14 +0100

    proxy to middleware bug again

--- trakeo/src/proxy.ts
+++ trakeo/src/proxy.ts
@@ -38,8 +38,8 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
-        '/((?!api/inngest|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         // Always run for API routes
-        '/(api(?!/inngest)|trpc)(.*)',
+        '/(api|trpc)(.*)',
     ],
 };
