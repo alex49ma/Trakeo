136a7443d790edc5e0c6be329b1f0f9b24cea998
Alejandro Martín <alejandrmartnaudn@gmail.com>
2026-01-31 23:43:42 +0100

    prisma error bug on prod check

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,12 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
 
+if (!connectionString) {
+    if (process.env.NODE_ENV === "production") {
+        throw new Error("DATABASE_URL is not defined in production environment");
+    }
+}
+
 const createPrismaClient = () => {
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
